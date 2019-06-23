1ee366fb5eaf3833c827db59f255dbd4752017e3
Bruno A. Caceres <brunocaceres@hotmail.com>
2019-06-23 18:10:03 -0400

    Habilitando UsuarioStatus com ENUM.

--- schema-query/projeto/data/db.js
+++ schema-query/projeto/data/db.js
@@ -5,7 +5,8 @@ const usuarios =
         nome: 'João Silva',
         email: 'jsilva@email.com',
         idade: 29,
-        perfil_id: 1
+        perfil_id: 1,
+        status: 'ATIVO'
         
     },
     {
@@ -13,14 +14,16 @@ const usuarios =
         nome: 'Rafael Junior',
         email: 'rjunior@email.com',
         idade: 31,
-        perfil_id: 2
+        perfil_id: 2,
+        status: 'INATIVO'
     },
     {
         id: 3,
         nome: 'Daniela Smith',
         email: 'dsmith@email.com',
         idade: 24,
-        perfil_id: 1
+        perfil_id: 1,
+        status: 'BLOQUEADO'
     }
 ]
 
