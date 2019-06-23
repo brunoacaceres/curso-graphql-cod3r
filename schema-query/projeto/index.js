035035782a3270edfa9e8b6748951d4303e8952b
Bruno A. Caceres <brunocaceres@hotmail.com>
2019-06-23 07:09:04 -0400

    Resolver Usuario para campos diferentes e para um tipo não existente.

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -3,14 +3,36 @@ const { ApolloServer, gql} = require('apollo-server')
 const typeDefs = gql`
     scalar Date
 
+    type Usuario {
+        id: ID
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+        blabla: String
+    }
+
     # Pontos de entrada da sua API!
     type Query {
-        ola: String
-        horaAtual: Date
+        ola: String!
+        horaAtual: Date!
+        usuarioLogado: Usuario
     }
 `
 
 const resolvers = {
+    // Criando Resolver para um campos de nomes diferentes Ex: salario e salario_real
+    Usuario: {
+        salario(usuario) {
+            return usuario.salario_real
+        },
+        
+        blabla() {
+            return 'Opa'
+        }        
+    },
+
     //Objeto Query
     Query: {
         ola(){
@@ -21,7 +43,20 @@ const resolvers = {
             // return data.toString();
             // return `${new Date}`
             return new Date
+        },
+        usuarioLogado(Objeto){
+            console.log(Objeto)
+            return {
+                id: 1,
+                nome: 'Gislene e Lorena',
+                email: 'GieLoly@email.com',
+                idade: 3,
+                salario_real: 9876.54,
+                vip: true
+
+            }
         }
+
     }
 }
 
