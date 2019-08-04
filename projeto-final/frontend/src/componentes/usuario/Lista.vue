c9ab9d19cdc96f9d8a182ab54d789ba159a9fb07
Bruno A. Caceres <brunocaceres@hotmail.com>
2019-08-04 11:13:54 -0400

    92. Configuração do Apollo Client

--- projeto-final/frontend/src/componentes/usuario/Lista.vue
+++ projeto-final/frontend/src/componentes/usuario/Lista.vue
@@ -48,7 +48,9 @@ export default {
     },
     methods: {
         obterUsuarios() {
-            // 
+            // console.log('obterUsuarios')
+            console.log(this.$api)
+       
         }
     }
 }
